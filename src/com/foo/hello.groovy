d4ce96eb692ecfbd24547a475dab5d5555fda4dd
sougatadas10 <sougatadas10@users.noreply.github.com>
2019-10-21 13:04:03 -0400

    Update hello.groovy

--- src/com/foo/hello.groovy
+++ src/com/foo/hello.groovy
@@ -1,14 +1,13 @@
 package com.foo
 
 class hello {
-    def title
+    String title
 
-   // hello(title) {
-    //    this.title = title
-    //}
+   public hello(String title) {
+       this.title = title
+    }
 
     String dis() {
-      //  System.out.println(title)
-        return "Hello"
+        return ("Hello, " + title)
     }
 }
