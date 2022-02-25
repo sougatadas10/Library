7ca772a8d23fa350dd80f35e82dfe80ea4c65b35
sougatadas10 <sougatadas10@users.noreply.github.com>
2022-02-25 17:07:09 +0000

    Update readYML.groovy

--- src/com/foo/readYML.groovy
+++ src/com/foo/readYML.groovy
@@ -1,8 +1,8 @@
 package com.foo
 
 class readYML {
-        void readYMLFile() {
-            //def envConfig = readYaml file: filePath
+        void readYMLFile(String filePath) {
+            def envConfig = readYaml file: filePath
 
             //envConfig.each {
             //    key,value -> println("Key: "+key+"Value: "+value)
