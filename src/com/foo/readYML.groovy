e04f1d311e1739528a034af41590f547f1af2fd0
sougatadas10 <sougatadas10@users.noreply.github.com>
2022-02-25 17:56:18 +0000

    Update readYML.groovy

--- src/com/foo/readYML.groovy
+++ src/com/foo/readYML.groovy
@@ -1,13 +1,13 @@
 package com.foo
 
 class readYML {
-        String parse(LinkedHashMap envConfig) {
+        String parse(String context,LinkedHashMap envConfig) {
             //def envConfig = readYaml file: filePath
 
             envConfig.each {
-                key,value -> println("Key: "+key+"Value: "+value)
+                key,value -> context.println("Key: "+key+"Value: "+value)
             }
-             return "hello" 
+            
         }
 
 }
