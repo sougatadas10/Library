3505645a0a14fd7cfd6ae3a240d6baab9c462dd0
Sougata Das <sougata.das@cognizant.com>
2022-02-25 18:43:36 +0000

    correct indentation

--- src/com/foo/readYML.groovy
+++ src/com/foo/readYML.groovy
@@ -1,19 +1,16 @@
 package com.foo
 
 class readYML {
-        void parse(def context,LinkedHashMap envConfig) {
-            //def envConfig = readYaml file: filePath
-
-            envConfig.each {
-                key,value -> 
-                    if (key == "mysql") {
-                            context.println(value.deploy)
-                            value.extra_vars.each {
-                                    k,v -> context.println ("k:" +k+" "+ "v:" +v)
-                            }
-                    }    
-            }
-            
+    void parse(def context,LinkedHashMap envConfig) {
+        envConfig.each {
+            key,value -> 
+            if (key == "mysql") {
+                context.println(value.deploy)
+                value.extra_vars.each {
+                    k,v -> context.println ("k:" +k+" "+ "v:" +v)
+                }
+            }    
         }
+    }
 
 }
