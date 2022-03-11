d6e83531158bf23789e5cb5d26952f4d2a0def8f
sougatadas10 <sougatadas10@users.noreply.github.com>
2022-03-11 19:47:44 +0530

    Update readYML.groovy

--- src/com/foo/readYML.groovy
+++ src/com/foo/readYML.groovy
@@ -28,7 +28,7 @@ class readYML {
                     k,v -> extraVars+k+": "+v
                 }
                 extraVars=extraVars+"}"
-                params= "[\$class: \'StringParameterValue\',"+ "name: extra_vars,"+"value: "+extraVars+"]"
+                params= params+"[\$class: \'StringParameterValue\',"+ "name: extra_vars,"+"value: "+extraVars+"]"
             }
         }
         return params
