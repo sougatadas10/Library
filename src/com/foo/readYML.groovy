cf1056b6c4b993fb29a39153f4981dadd70dd0c6
sougatadas10 <sougatadas10@users.noreply.github.com>
2022-03-11 23:35:26 +0530

    Update readYML.groovy

--- src/com/foo/readYML.groovy
+++ src/com/foo/readYML.groovy
@@ -19,23 +19,19 @@ class readYML {
     }
 
 //    Map setParams(LinkedHashMap config) {
-    ArrayList setParams(LinkedHashMap config) {    
-        //String params="[",extraVars="{"
-        def params=[]
-        String extraVars="{"
+      String setParams(LinkedHashMap config) {    
+        String params="[",extraVars="{"
         
         config.each {
             key,value ->
             if (key != "extra_vars")
-                //params= params+"[\$class: \'StringParameterValue\',"+ "name: "+key+","+"value: "+value+"],"
-                params.add([$class:'StringParameterValue',name:key,value: value])
+                params= params+"[\$class: \'StringParameterValue\',"+ "name: "+key+","+"value: "+value+"],"
             else {
                 value.each {
                     k,v -> extraVars=extraVars+k+": "+v+","
                 }
                 extraVars=extraVars.substring(0, extraVars.length()-1) +"}"
-                //params= params+"[\$class: \'StringParameterValue\',"+ "name: extra_vars,"+"value: "+extraVars+"]"
-                params= params.add([$class:'StringParameterValue',name: "extra_vars",value: extraVars])
+                params= params+"[\$class: \'StringParameterValue\',"+ "name: extra_vars,"+"value: "+extraVars+"]"
                 
             }
         }
