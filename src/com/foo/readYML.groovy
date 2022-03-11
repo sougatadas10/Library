ec1ff62a40af96696e2a5e5426c67057fb66ed72
Sougata Das <sougata.das@cognizant.com>
2022-03-11 13:30:49 +0000

    add recursive parsing logic

--- src/com/foo/readYML.groovy
+++ src/com/foo/readYML.groovy
@@ -5,12 +5,31 @@ class readYML {
         envConfig.each {
             key,value -> 
             if (key == "mysql") {
-                context.println(value.deploy)
-                value.extra_vars.each {
-                    k,v -> context.println ("k:" +k+" "+ "v:" +v)
-                }
+                context.println(setParams(value))
+                //context.println(value.deploy)
+                //value.extra_vars.each {
+                //    k,v -> context.println ("k:" +k+" "+ "v:" +v)
+
+                //}
             }    
         }
+
     }
 
+    String setParams(LinkedHashMap config) {
+        String params="[",extraVars="{"
+        config.each {
+            key,value ->
+            if (key != "extra_vars")
+                params= "[$class: 'StringParameterValue',"+ "name: "+key+","+"value: "+value+"]"
+            else {
+                key.extra_vars.each {
+                    k,v -> extraVars+k+": "+v
+                }
+                extraVars=extraVars+"}"
+                params= "[$class: 'StringParameterValue',"+ "name: extra_vars,"+"value: "+extraVars+"]"
+            }
+        }
+        return params
+    }
 }
