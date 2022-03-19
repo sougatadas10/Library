43658f0955a3f33b5fbfd0f14b4d56e550e06982
sougatadas10 <sougatadas10@users.noreply.github.com>
2022-03-19 15:45:45 +0530

    Update DBConnection.groovy

--- src/com/foo/DBConnection.groovy
+++ src/com/foo/DBConnection.groovy
@@ -5,6 +5,9 @@ import groovy.sql.Sql;
 import java.util.ServiceLoader;
 import java.sql.Driver;
 import java.sql.DriverManager;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource
+import com.mysql.jdbc.*
+import groovy.sql.*
 
 class DBConnection{
   
@@ -13,10 +16,16 @@ class DBConnection{
     //Class.forName("com.mysql.jdbc.Driver")
     //DriverManager.registerDriver(new com.mysql.jdbc.Driver())
     //ServiceLoader<Driver> loader = ServiceLoader.load(Driver.class);
-    d=Class.forName("com.mysql.jdbc.Driver").newInstance()
-    context.println d.class // class com.mysql.jdbc.Driver
-    def sql = Sql.newInstance("jdbc:mysql://localhost:3306/employee", "root","root@pass", "com.mysql.jdbc.Driver")
+    //d=Class.forName("com.mysql.jdbc.Driver").newInstance()
+    //context.println d.class // class com.mysql.jdbc.Driver
+    //def sql = Sql.newInstance("jdbc:mysql://localhost:3306/employee", "root","root@pass", "com.mysql.jdbc.Driver")
     //def rows = sql.execute "select count(*) from test_table;"
     //echo rows.dump()
+    MysqlDataSource ds = new MysqlDataSource()
+    ds.user = 'root'
+    ds.password = "root@pass"
+    ds.url = 'jdbc:mysql://localhost:3306/test'
+
+    Sql sql=Sql.newInstance(ds)
   }
 }
