9b1bb4c6906879e499532bbf3ac5972bd0f96a35
sougatadas10 <sougatadas10@users.noreply.github.com>
2022-03-19 21:27:24 +0530

    Update DBConnection.groovy

--- src/com/foo/DBConnection.groovy
+++ src/com/foo/DBConnection.groovy
@@ -1,21 +1,22 @@
 package com.foo
-@Grab( 'mysql:mysql-connector-java:8.0.28' )
-//@GrabConfig(systemClassLoader=true)
-import groovy.sql.Sql;
-//import com.mysql.jdbc.jdbc2.optional.MysqlDataSource
-import com.mysql.jdbc.*
+@Grab( 'mysql:mysql-connector-java:5.1.27' )
 
+import groovy.sql.Sql;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource
+import com.mysql.jdbc.*
 class DBConnection{
   
   def getConnection(def context) {
-    //MysqlDataSource ds = new MysqlDataSource()
-    //ds.user = 'root'
-    //ds.password = 'root@pass'
-    //ds.url = 'jdbc:mysql://127.0.0.1:3306/employee'
-    //Sql sql=Sql.newInstance(ds)
+    MysqlDataSource ds = new MysqlDataSource()
+    ds.user = 'root'
+    ds.password = 'root@pass'
+    ds.url = 'jdbc:mysql://127.0.0.1:3306/employee' 
+    
+    
+    
+    Sql sql=Sql.newInstance(ds)
     //def rows = sql.execute "select count(*) from employee.employee_master;"
     //context.println rows.dump()
-    def sql = Sql.newInstance('jdbc:mysql://localhost:3306/employee','root', 'root@pass', 'com.mysql.jdbc.Driver')
     sql.query('SELECT employee_id, employee_name FROM employee_master') { 
       resultSet ->
       while (resultSet.next()) {
@@ -24,4 +25,3 @@ class DBConnection{
       }
     }
   }
-}
