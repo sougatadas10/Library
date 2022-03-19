77319da283c8bd0d3e167e0a279b05ce7f554d9e
sougatadas10 <sougatadas10@users.noreply.github.com>
2022-03-20 00:03:23 +0530

    Update DBConnection.groovy

--- src/com/foo/DBConnection.groovy
+++ src/com/foo/DBConnection.groovy
@@ -4,21 +4,22 @@ package com.foo
 
 import groovy.sql.Sql;
 //import com.mysql.jdbc.jdbc2.optional.MysqlDataSource
+imort org.mysql.jdbc.JDBCDataSource
 import com.mysql.jdbc.*
 class DBConnection{
   
   def getConnection(def context) {
-    /**MysqlDataSource ds = new MysqlDataSource()
+    MysqlDataSource ds = new MysqlDataSource()
     ds.user = 'root'
     ds.password = 'root@pass'
     ds.url = 'jdbc:mysql://host.docker.internal:3306/employee?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull&serverTimezone=GMT"'  
-    Sql sql=Sql.newInstance(ds)**/
+    Sql sql=Sql.newInstance(ds)
     
-    def url = 'jdbc:mysql:host.docker.internal:3306:employee'
+    /**def url = 'jdbc:mysql:host.docker.internal:3306:employee'
     def user = 'root'
     def password = 'root@pass'
     def driver = 'org.mysql.cj.jdbcDriver'
-    def sql = Sql.newInstance(url, user, password, driver)
+    def sql = Sql.newInstance(url, user, password, driver)**/
     
     sql.firstRow('SELECT employee_id, employee_name FROM employee_master')
     
