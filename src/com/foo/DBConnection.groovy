2d8a923c90a3b3f19e16c20c6d2f1002c5e433c3
sougatadas10 <sougatadas10@users.noreply.github.com>
2022-03-20 00:06:59 +0530

    Update DBConnection.groovy

--- src/com/foo/DBConnection.groovy
+++ src/com/foo/DBConnection.groovy
@@ -1,16 +1,16 @@
 package com.foo
-//@Grab( 'mysql:mysql-connector-java:5.1.27' )
-@Grab( 'mysql:mysql-connector-java:8.0.11')
+@Grab( 'mysql:mysql-connector-java:5.1.27' )
+//@Grab( 'mysql:mysql-connector-java:8.0.11')
 
 import groovy.sql.Sql;
-//import com.mysql.jdbc.jdbc2.optional.MysqlDataSource
-import org.mysql.jdbc.JDBCDataSource
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource
+//import org.mysql.jdbc.JDBCDataSource
 import com.mysql.jdbc.*
 class DBConnection{
   
   def getConnection(def context) {
-    //MysqlDataSource ds = new MysqlDataSource()
-    JDBCDataSource ds = new JDBCDataSource()
+    MysqlDataSource ds = new MysqlDataSource()
+    //JDBCDataSource ds = new JDBCDataSource()
     ds.user = 'root'
     ds.password = 'root@pass'
     ds.url = 'jdbc:mysql://host.docker.internal:3306/employee?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull&serverTimezone=GMT"'  
