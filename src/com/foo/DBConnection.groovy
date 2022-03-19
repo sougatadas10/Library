1ae63f82ed50cdb22e60bcc7d48f96687ab43dcd
sougatadas10 <sougatadas10@users.noreply.github.com>
2022-03-19 15:43:09 +0530

    Update DBConnection.groovy

--- src/com/foo/DBConnection.groovy
+++ src/com/foo/DBConnection.groovy
@@ -14,7 +14,7 @@ class DBConnection{
     //DriverManager.registerDriver(new com.mysql.jdbc.Driver())
     //ServiceLoader<Driver> loader = ServiceLoader.load(Driver.class);
     d=Class.forName("com.mysql.jdbc.Driver").newInstance()
-    sh.println d.class // class com.mysql.jdbc.Driver
+    context.println d.class // class com.mysql.jdbc.Driver
     def sql = Sql.newInstance("jdbc:mysql://localhost:3306/employee", "root","root@pass", "com.mysql.jdbc.Driver")
     //def rows = sql.execute "select count(*) from test_table;"
     //echo rows.dump()
