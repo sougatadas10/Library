7b9f11f86a61c9927b415973aa1c58856579a22a
sougatadas10 <sougatadas10@users.noreply.github.com>
2022-03-20 00:11:54 +0530

    Update DBConnection.groovy

--- src/com/foo/DBConnection.groovy
+++ src/com/foo/DBConnection.groovy
@@ -1,26 +1,28 @@
 package com.foo
-@Grab( 'mysql:mysql-connector-java:5.1.27' )
-//@Grab( 'mysql:mysql-connector-java:8.0.11')
+//@Grab( 'mysql:mysql-connector-java:5.1.27' )
+@Grab( 'mysql:mysql-connector-java:8.0.20')
 
 import groovy.sql.Sql;
-import com.mysql.jdbc.jdbc2.optional.MysqlDataSource
-//import org.mysql.jdbc.JDBCDataSource
+//import com.mysql.jdbc.jdbc2.optional.MysqlDataSource
+import org.mysql.jdbc.JDBCDataSource
 import com.mysql.jdbc.*
 class DBConnection{
   
   def getConnection(def context) {
-    MysqlDataSource ds = new MysqlDataSource()
+    //MysqlDataSource ds = new MysqlDataSource()
     //JDBCDataSource ds = new JDBCDataSource()
-    ds.user = 'root'
-    ds.password = 'root@pass'
-    ds.url = 'jdbc:mysql://host.docker.internal:3306/employee?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull&serverTimezone=GMT"'  
-    Sql sql=Sql.newInstance(ds)
+    //ds.user = 'root'
+    //ds.password = 'root@pass'
+    //ds.url = 'jdbc:mysql://host.docker.internal:3306/employee?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull&serverTimezone=GMT"'  
+    //Sql sql=Sql.newInstance(ds)
     
     /**def url = 'jdbc:mysql:host.docker.internal:3306:employee'
     def user = 'root'
     def password = 'root@pass'
     def driver = 'org.mysql.cj.jdbcDriver'
     def sql = Sql.newInstance(url, user, password, driver)**/
+    
+    def sql = Sql.newInstance("jdbc:mysql://host.docker.internal:3306/test", "root", "root", "com.mysql.jdbc.Driver")
     
     sql.firstRow('SELECT employee_id, employee_name FROM employee_master')
     
