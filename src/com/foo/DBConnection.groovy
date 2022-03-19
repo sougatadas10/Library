9818757da697876d3eb93505bd9e6ae21eceb9af
sougatadas10 <sougatadas10@users.noreply.github.com>
2022-03-19 21:28:40 +0530

    Update DBConnection.groovy

--- src/com/foo/DBConnection.groovy
+++ src/com/foo/DBConnection.groovy
@@ -10,18 +10,9 @@ class DBConnection{
     MysqlDataSource ds = new MysqlDataSource()
     ds.user = 'root'
     ds.password = 'root@pass'
-    ds.url = 'jdbc:mysql://127.0.0.1:3306/employee' 
-    
-    
-    
+    ds.url = 'jdbc:mysql://127.0.0.1:3306/employee'  
     Sql sql=Sql.newInstance(ds)
-    //def rows = sql.execute "select count(*) from employee.employee_master;"
-    //context.println rows.dump()
-    sql.query('SELECT employee_id, employee_name FROM employee_master') { 
-      resultSet ->
-      while (resultSet.next()) {
-        def first = resultSet.getString(1)
-        def last = resultSet.getString('employee_name')
-      }
+    
+   
     }
   }
