3d132330ab5663e51cd50f04fdc64550a5123016
sougatadas10 <sougatadas10@users.noreply.github.com>
2022-03-31 12:44:56 +0000

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -11,8 +11,8 @@ def call(Map params) {
         ])  
       }
       stage('build') {
-        def mavenHome=tool name: 'test-maven', type: 'maven'
-        def jdkHome=tool name: 'test-jdk', type: 'jdk'
+        def mavenHome=${context.tool 'test-maven'}
+        def jdkHome=${context.tool'test-jdk'}
         withMaven(jdk: jdkHome, maven: mavenHome) {
             sh "mvn -f ${params.file} ${params.options}"
           }
