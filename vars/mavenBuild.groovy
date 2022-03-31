ac7c4a3d38280d98d53f31d0cc21c9366ec53e11
sougatadas10 <sougatadas10@users.noreply.github.com>
2022-03-31 11:22:04 +0000

    add node to the method

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,16 +1,18 @@
 #!/usr/bin/env groovy
 
 def call(Map params) {
-  stage('clone') {
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name: params.branch ]],
-        userRemoteConfigs: [[ url: params.url ]]
-      ])  
-    }
-    stage('build') {
-        withMaven(jdk: 'test-jdk', maven: 'test-maven') {
-          sh "mvn -f ${params.file} ${params.options}"
-        }
-    }
+  node() {
+    stage('clone') {
+      checkout([
+          $class: 'GitSCM',
+          branches: [[name: params.branch ]],
+          userRemoteConfigs: [[ url: params.url ]]
+        ])  
+      }
+      stage('build') {
+          withMaven(jdk: 'test-jdk', maven: 'test-maven') {
+            sh "mvn -f ${params.file} ${params.options}"
+          }
+      }
+    }  
 } 
